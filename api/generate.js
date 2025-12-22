bb776256697952b6b3bce00cb7033e28bbbad2a9
Testando0 <joh769637@gmail.com>
2025-12-22 14:28:10 -0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
     try {
         const response = await fetch(
-            "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell",
+            "https://router-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell",
             {
                 headers: { 
                     Authorization: `Bearer ${HF_TOKEN}`,
